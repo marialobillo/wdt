8440ae2b21e51368b80ceeefbbb82060e3f31c2c
Maria Lobillo-Santos <maria.lobillo.santos@gmail.com>
2018-07-11 18:55:47 +0200

    medium way

---
+++ become_dev/preparing/medium.js
@@ -0,0 +1,18 @@
+
+function write_song(){
+  let beers = 99;
+
+  for(let i = 99; i > 0; i--){
+    if(i != 1){
+      console.log(i + ' bottles of beer on the wall, ' + i + ' bottles of beer.');
+      console.log('Take one down and pass it around, ' + (i - 1) + ' bottles of beer on the wall.');
+    } else {
+      console.log(i + ' bottle of beer on the wall, ' + i + ' bottle of beer.');
+      console.log('Take one down and pass it around, no more bottles of beer on the wall.');
+      console.log('No more bottles of beer on the wall, no more bottler of beer.');
+      console.log('Got to the store and buy some more, 99 bottles of beer on the wall.');
+    }
+  }
+}
+
+write_song();
